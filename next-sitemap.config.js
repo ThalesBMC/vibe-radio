ab1f87032e66d477dbe2c1fd17a5452216a1dbf0
Thales Brederodes <thalesbmc@gmail.com>
2025-04-26 13:39:04 -0300

    feat: adds next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://viberadio.live",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  outDir: "public",
+  priority: 1.0,
+  changefreq: "daily",
+};
